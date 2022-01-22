b3b37a24856106530dc49fc670b3c9155bf5a412
jscardona42 <35873185+jscardona42@users.noreply.github.com>
2022-01-22 13:29:43 -0500

    Configuración de conexión a Postgres

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,34 @@
 import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { OrdersModule } from './orders/orders.module';
+import { ClientsModule } from './clients/clients.module';
+import { TechniciansModule } from './technicians/technicians.module';
+import { TechniciansOrdersModule } from './technicians-orders/technicians-orders.module';
+import { ConfigModule } from '@nestjs/config';
 
 @Module({
-  imports: [],
+  imports: [
+    ConfigModule.forRoot({
+      isGlobal: true,
+    }),
+    TypeOrmModule.forRoot({
+      type: 'postgres',
+      host: process.env.POSTGRES_HOST,
+      port: Number(process.env.POSTGRES_PORT),
+      username: process.env.POSTGRES_USER,
+      password: process.env.POSTGRES_PASSWORD,
+      database: process.env.POSTGRES_DB,
+      autoLoadEntities: true,
+      synchronize: true,
+    }),
+    OrdersModule,
+    ClientsModule,
+    TechniciansModule,
+    TechniciansOrdersModule,
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
-export class AppModule {}
+export class AppModule { }
