ec859eab3c8b36d355186d29648a0075ccd21581
jscardona42 <35873185+jscardona42@users.noreply.github.com>
2022-01-22 15:46:30 -0500

    Configuración para despliegue

--- src/main.ts
+++ src/main.ts
@@ -19,6 +19,6 @@ async function bootstrap() {
     .build();
   const document = SwaggerModule.createDocument(app, options);
   SwaggerModule.setup('api', app, document);
-  await app.listen(3000 || process.env.PORT);
+  await app.listen(process.env.PORT || 3000);
 }
 bootstrap();
